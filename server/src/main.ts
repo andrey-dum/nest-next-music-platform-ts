d90cb21285f807399a75311ceae5f3dc09b296ac
andrey-dum <duminskiy@gmail.com>
2021-02-25 20:13:23 +0200

    track

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// async function bootstrap() {
-//   const PORT = process.env.PORT || 5000;
-
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(3000);
-// }
-// bootstrap();
 
 const start = async () => {
   try {
