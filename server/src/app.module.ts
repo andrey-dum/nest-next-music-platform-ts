c0b0b86a8d469f5ee4c38204019e8e4224686824
andrey-dum <duminskiy@gmail.com>
2021-02-27 13:21:38 +0200

    FilesUpload, search, pagination

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,17 @@
+import { MONGODB } from './config';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TrackModule } from './track/track.module';
+import {FileModule} from "./file/file.module";
+import * as path from 'path'
+import {ServeStaticModule} from "@nestjs/serve-static";
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.oeudk.mongodb.net/music-platform?retryWrites=true&w=majority'),
-    TrackModule
+    ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
+    MongooseModule.forRoot(MONGODB),
+    TrackModule,
+    FileModule
   ],
 })
 export class AppModule {}
